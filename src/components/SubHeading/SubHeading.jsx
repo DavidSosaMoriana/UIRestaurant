9d8985a3243dd2222eaddf5f7f39097c50017ec3
David Sosa <david.sosa@singularfactory.com>
2022-02-04 15:34:45 +0000

    Header & subheading Changes

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
-const SubHeading = () => (
+import { images } from '../../constants'
+
+const SubHeading = ({ title }) => (
   <div>
-    SubHeading
+    <p className="p__cormorant">{title}</p>
+    <img src={images.spoon} alt="spoon_image" className="spoon__img" />
   </div>
 );
 
